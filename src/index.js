1ddffbd99d2dc794d786768a764eb16416034467
AnastasiyaMazura <mazura.anastasiya@gmail.com>
2018-09-24 16:23:20 +0300

    all tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let count = 0, i = 5;
+  while (i <= number){
+      count += Math.trunc(number / i);
+      i *= 5;
+  }
+  return count;
 }
